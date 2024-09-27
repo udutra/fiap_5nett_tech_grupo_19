73dbc777df671c5e1690a8c4cfadecac52ecd16d
Guilherme Galarça Dutra <udutra@gmail.com>
2024-09-27 14:00:56 -0300

    Ajsute da classe program.

--- fiap_5nett_tech.api/Program.cs
+++ fiap_5nett_tech.api/Program.cs
@@ -7,59 +7,53 @@ using fiap_5nett_tech.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-namespace fiap_5nett_tech.api;
+var builder = WebApplication.CreateBuilder(args);
 
-public abstract class Program
+if (builder.Environment.IsEnvironment("Testing"))
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        if (builder.Environment.IsEnvironment("Testing"))
-        {
-            // Usa banco de dados em memória para testes
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseInMemoryDatabase("InMemoryDbForTesting"));
-        }
-        else
-        {
-            // Usa banco de dados SQL Server para desenvolvimento/produção
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-        }
-
-        builder.Services.AddScoped<IContactInterface, ContactService>();
-        builder.Services.AddScoped<IContactRepository, ContactRepository>();
-        builder.Services.AddScoped<IRegionRepository, RegionRepository>();
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tech Challenge 1", Version = "" });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            }
-        );
-
-        var app = builder.Build();
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.MapGet("/", () => "Hello World!");
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.Run();
-    }
+    // Usa banco de dados em memória para testes
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseInMemoryDatabase("InMemoryDbForTesting"));
 }
+else
+{
+    // Usa banco de dados SQL Server para desenvolvimento/produção
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
+
+builder.Services.AddScoped<IContactInterface, ContactService>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tech Challenge 1", Version = "" });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+    }
+);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
+
+public partial class Program { }
